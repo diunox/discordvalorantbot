9a691660a713ea5694a8b1ab6ace3938b7ccaab0
Austin Black <austinblack1@gmail.com>
2022-02-11 22:48:41 -0600

    change pl

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const prefix = "!";
 
 const maps = ["ascent", "bind", "breeze", "fracture", "haven", "icebox", "split"]
 
-var players = ["Dewie", "Darth", "cicada mojo", "Direktor", "Ressk"]
+var players = ["Dewie", "Darth", "cicada mojo", "Direktor", "Ehsea"]
 
 const pistolbase = [
   { rating: 9, title: 'Walk push A' },
@@ -19,8 +19,8 @@ const pistolbase = [
   { rating: 6, title: 'Hard rush B' },
   { rating: 8, title: 'Darth has been selected as shotcaller this round!' },
   { rating: 8, title: 'Dewie has been selected as shotcaller this round!' },
-  { rating: 8, title: 'cicada mojo has been selected as shotcaller this round!' },
-  { rating: 8, title: 'Ressk has been selected as shotcaller this round!' },
+  { rating: 8, title: 'cicada mojo has been selected as shotcaller this round!' },~
+  { rating: 8, title: 'Ehsea has been selected as shotcaller this round!' },
   { rating: 8, title: 'Direktor has been selected as shotcaller this round!' },
   { rating: 3.5, title: '4/1 split A/B, solo distracts for 30s' },
   { rating: 3.5, title: '4/1 split B/A, solo distracts for 30s' },
@@ -36,7 +36,7 @@ const econbase = [
   { rating: 8, title: 'Darth has been selected as shotcaller this round!' },
   { rating: 8, title: 'Dewie has been selected as shotcaller this round!' },
   { rating: 8, title: 'cicada mojo has been selected as shotcaller this round!' },
-  { rating: 8, title: 'Ressk has been selected as shotcaller this round!' },
+  { rating: 8, title: 'Ehsea has been selected as shotcaller this round!' },
   { rating: 8, title: 'Direktor has been selected as shotcaller this round!' },
   { rating: 5, title: 'Hard rush A' },
   { rating: 5, title: 'Hard rush B' },
@@ -56,7 +56,7 @@ const fullbase = [
   { rating: 8, title: 'Darth has been selected as shotcaller this round!' },
   { rating: 8, title: 'Dewie has been selected as shotcaller this round!' },
   { rating: 8, title: 'cicada mojo has been selected as shotcaller this round!' },
-  { rating: 8, title: 'Ressk has been selected as shotcaller this round!' },
+  { rating: 8, title: 'Ehsea has been selected as shotcaller this round!' },
   { rating: 8, title: 'Direktor has been selected as shotcaller this round!' },
   { rating: 5, title: 'Hard rush A' },
   { rating: 5, title: 'Hard rush B' },
