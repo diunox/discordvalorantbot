0c70b790be595254cf21d77e26e563810499cdba
Austin Black <ablack@digitalocean.com>
2022-03-02 17:52:22 -0600

    update default roster

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 
 const prefix = "!";
 // globals go here because it's a bot with no concept of state
-var players = ["Dewie", "Ehsea", "cicada mojo", "Direktor", "Ressk"];
+var players = ["Dewie", "Ehsea", "cicadamojo", "Direktor", "Cretzl"];
 var currentMap = "ascent";
 var plays = [];
 var pistolplays = "";
