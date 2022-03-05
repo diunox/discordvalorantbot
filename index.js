9ab49a89cf6bdb0dcad87a7a01dc360e63e7596c
Austin Black <austinblack1@gmail.com>
2022-03-04 22:02:17 -0600

    fix default map

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 
 const prefix = "!";
 // globals go here because it's a bot with no concept of state
-var players = ["Dewie", "Ehsea", "cicada mojo", "Direktor", "Ressk"];
-var currentMap = "not set";
+var players = ["Dewie", "Ehsea", "AnMex", "Direktor", "Ressk"];
+var currentMap = "Ascent";
 // Note that the base set is NOT included for Fracture
 
 function loadPlays(currentMap) {
